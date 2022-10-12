8c2b7abad15cdab3f6b924453662dea6bccb0e9d
Md Monirul Islam <mmimonir.ctg@gmail.com>
2022-10-12 21:00:33 +0600

    project recreate

---
+++ src/navbar/auth.jsx
@@ -0,0 +1,44 @@
+import { Link, Route, Routes } from "react-router-dom";
+import AuthUser from "../components/AuthUser";
+import Dashboard from "../components/dashboard";
+import Home from "../components/home";
+
+function Auth() {
+  const { token, logout } = AuthUser();
+  const logoutUser = () => {
+    if (token != undefined) {
+      logout();
+    }
+  };
+  return (
+    <>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link className="nav-link" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link className="nav-link" to="/dashboard">
+              Dashboard
+            </Link>
+          </li>
+          <li className="nav-item">
+            <span role="button" className="nav-link" onClick={logoutUser}>
+              Logout
+            </span>
+          </li>
+        </ul>
+      </nav>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default Auth;
